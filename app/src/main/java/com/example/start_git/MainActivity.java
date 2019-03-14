f9d6c8691173bf871e1e74d4e370b7ccd69f1037
valentinanyaz <valya.kn.work@gmail.com>
2019-03-14 10:59:35 +0300

    Button

--- app/src/main/java/com/example/start_git/MainActivity.java
+++ app/src/main/java/com/example/start_git/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.start_git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
