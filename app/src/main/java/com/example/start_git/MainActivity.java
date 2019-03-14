b429721fb74455b9fa1ef6a051d1a5974325deab
valentinanyaz <valya.kn.work@gmail.com>
2019-03-14 10:55:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/start_git/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.start_git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
